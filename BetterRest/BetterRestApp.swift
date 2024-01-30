b3a0076611dcc76a4fe133401d7582962f426fa8
Djroton Noé Sossou <70499809+noahdjroton@users.noreply.github.com>
2024-01-30 22:36:04 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Djroton Noé SOSSOU on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
